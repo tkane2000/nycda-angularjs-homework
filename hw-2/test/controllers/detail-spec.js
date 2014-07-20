18f16e663dc080b08f4563343951faeaeffc5f33
tkane2000 <tkthompson@gmail.com>
2014-07-20 14:04:51 -0400

    got detailed test to work and added test for question status

--- hw-2/test/controllers/detail-spec.js
+++ hw-2/test/controllers/detail-spec.js
@@ -5,19 +5,33 @@ describe("Detail", function () {
     beforeEach(module('questions'));
     beforeEach(inject(function ($rootScope, $controller) {
         $scope = $rootScope.$new();
+        $scope.statusMessages = {
+            unanswered: 'unanswered',
+            answeredCorrectly: 'Correct!',
+            answeredIncorrectly: 'Wrong! :('
+        };
         controller = $controller('Detail', {$scope: $scope});
     }));
 
     describe("onSelectAnswer", function () {
         var question;
+        var option = {
+            value: "Mike",
+            correct: true
+        };
 
         beforeEach(function () {
             question = {question: "What is your name?"};
         });
 
         it("Should set the answer property of the question to the option provided", function () {
-            $scope.onSelectAnswer(question, "Mike");
-            expect(question.answer).toEqual("Mike");
+            $scope.onSelectAnswer(question, option);
+            expect(question.answer).toEqual(option);
+        });
+        
+        it("Should set the question's status message to let us know we answered correctly", function () {
+            $scope.onSelectAnswer(question, option);
+            expect(question.status).toEqual('Correct!'); // TODO: grab value from Data.statusMessages (or do we need to Mock this?)
         });
 
     });
