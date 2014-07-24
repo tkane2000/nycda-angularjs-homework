b074645e7e9b8803a2bee8beee287b0ea8ebba3b
tkane2000 <tkthompson@gmail.com>
2014-07-24 16:45:41 -0400

    adjust when image url is undefined, ngCloak directive on results, factory, constant, etc

--- hw-3/app/scripts/app.js
+++ hw-3/app/scripts/app.js
@@ -16,4 +16,10 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ]);
+  ]).constant('_', window._) // TODO: Is this really best practice?
+  .constant('nyTimesConfig', {
+    baseUri: 'http://api.nytimes.com/svc/search/v2/articlesearch.json',
+    queryString: 'api-key=052861d142cf4eb7fa12bb79781fdbe1:11:69591426&response-format=json'
+  });
+
+
--- hw-3/app/scripts/controllers/search.js
+++ hw-3/app/scripts/controllers/search.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// TODO: move to different file
+angular.module('hw3App').factory('nyTimesQuery', ['$http', 'nyTimesConfig',
+  function($http, config){
+    return {
+      getFeed: function(query) {
+        console.log('uri + search.query: ' + config.uri + query);
+        return $http({
+          url: config.baseUri + '?' + config.queryString + '&q=' + query,
+          method: 'GET'
+        });
+      }
+    };
+  }]);
+
 /**
  * @ngdoc function
  * @name hw3App.controller:SearchCtrl
@@ -8,6 +22,23 @@
  * Controller of the hw3App
  */
 angular.module('hw3App')
-  .controller('SearchCtrl', function ($scope) {
+  .controller('SearchCtrl', ['$scope', '_', 'nyTimesQuery',
+    function ($scope, _, queryFactory) {
 
-  });
+      var update = function (data) { // , status, headers, config
+        $scope.results = data;
+      };
+
+      var _search = function(search){
+        console.log('search.query: ' + search.query);
+
+          queryFactory.getFeed(search.query)
+            .success(update)
+            .error(function(data) { // , status, headers, config
+              console.log('error: data: ' + data);
+            });
+      };
+
+      $scope.onSearch = _.debounce(_search, 500);
+  }]);
+
