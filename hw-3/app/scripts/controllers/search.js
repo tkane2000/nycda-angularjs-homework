6aa09d57a87cac8b51c39da0efae6e8b98f1a50d
tkane2000 <tkthompson@gmail.com>
2014-07-29 21:40:57 -0400

    moved factory to it's own file

--- hw-3/app/scripts/controllers/search.js
+++ hw-3/app/scripts/controllers/search.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// TODO: move to different file
-angular.module('hw3App').factory('nyTimesQuery', ['$http', 'nyTimesConfig',
-  function($http, config){
-    return {
-      getFeed: function(query) {
-        console.log('uri + search.query: ' + config.uri + query);
-        return $http({
-          url: config.baseUri + '?' + config.queryString + '&q=' + query,
-          method: 'GET'
-        });
-      }
-    };
-  }]);
-
 /**
  * @ngdoc function
  * @name hw3App.controller:SearchCtrl
---
+++ hw-3/app/scripts/services/search-query.js
@@ -0,0 +1,15 @@
+angular.module('hw3App').factory('nyTimesQuery', ['$http', 'nyTimesConfig',
+  function($http, config){
+    'use strict';
+
+    var uri = config.baseUri + '?' + config.queryString + '&q=';
+
+    return {
+      getFeed: function(query) {
+        return $http({
+          url: uri + query,
+          method: 'GET'
+        });
+      }
+    };
+  }]);
